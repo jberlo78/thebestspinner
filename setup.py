143c3dab5066d1781518fb1c2924380f5a297f63
Matej Cotman <matejc@kiberpipa.org>
2012-11-07 12:04:41 +0100

    add setup file

---
+++ setup.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""Installer for this package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+import os
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = \
+    read('README.rst') + \
+    read('docs', 'HISTORY.rst') + \
+    read('docs', 'LICENSE.rst')
+
+setup(
+    name='tbsab',
+    version="1.0",
+    description="Python bindings for TheBestSpinner API",
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+    keywords='API spinner TheBestSpinner',
+    author='Peter Flood',
+    author_email='info@whywouldwe.com',
+    url='http://bitbucket.org/johnsmith/thebestspinner',
+    license='MIT',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'phpserialize'
+    ],
+    extras_require={
+        # list libs needed for unittesting this project
+        'test': [
+            'mock',
+            'unittest2',
+        ],
+        # list libs needed for releasing this project
+        'release': [
+            'zest.releaser',   # bin/longtest
+            'jarn.mkrelease',  # bin/mkrelease
+        ],
+    },
+)
